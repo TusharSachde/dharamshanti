77e5d574b1406cca2063969a2da184134c0fc37d
sohan1 <shn619@gmail.com>
2016-07-13 13:07:29 +0530

    chnages for movies

--- js/app.js
+++ js/app.js
@@ -14,60 +14,60 @@ firstapp.config(function($stateProvider, $urlRouterProvider, $httpProvider, $loc
   $httpProvider.defaults.withCredentials = true;
   $stateProvider
     .state('home', {
-    url: "/",
-    templateUrl: "views/template.html",
-    controller: 'HomeCtrl'
-  })
+      url: "/",
+      templateUrl: "views/template.html",
+      controller: 'HomeCtrl'
+    })
     .state('movies', {
-    url: "/movies",
-    templateUrl: "views/template.html",
-    controller: 'MoviesCtrl'
-  })
+      url: "/movies",
+      templateUrl: "views/template.html",
+      controller: 'MoviesCtrl'
+    })
     .state('awards', {
-    url: "/",
-    templateUrl: "views/template.html",
-    controller: 'AwardsCtrl'
-  })
+      url: "/",
+      templateUrl: "views/template.html",
+      controller: 'AwardsCtrl'
+    })
     .state('dharma-tv', {
-    url: "/dharma-tv",
-    templateUrl: "views/template.html",
-    controller: 'DharmaTvCtrl'
-  })
+      url: "/dharma-tv",
+      templateUrl: "views/template.html",
+      controller: 'DharmaTvCtrl'
+    })
     .state('tv-inside', {
-    url: "/tv-inside",
-    templateUrl: "views/template.html",
-    controller: 'TvInsideCtrl'
-  })
+      url: "/tv-inside",
+      templateUrl: "views/template.html",
+      controller: 'TvInsideCtrl'
+    })
     .state('dharma-world', {
-    url: "/dharma-world",
-    templateUrl: "views/template.html",
-    controller: 'DharmaWorldCtrl'
-  })
+      url: "/dharma-world",
+      templateUrl: "views/template.html",
+      controller: 'DharmaWorldCtrl'
+    })
     .state('news-events', {
-    url: "/news-events",
-    templateUrl: "views/template.html",
-    controller: 'NewsEventsCtrl'
-  })
+      url: "/news-events",
+      templateUrl: "views/template.html",
+      controller: 'NewsEventsCtrl'
+    })
     .state('dharma-journey', {
-    url: "/dharma-journey",
-    templateUrl: "views/template.html",
-    controller: 'DharmaJourneyCtrl'
-  })
+      url: "/dharma-journey",
+      templateUrl: "views/template.html",
+      controller: 'DharmaJourneyCtrl'
+    })
     .state('contact-us', {
-    url: "/contact-us",
-    templateUrl: "views/template.html",
-    controller: 'ContactUsCtrl'
-  })
+      url: "/contact-us",
+      templateUrl: "views/template.html",
+      controller: 'ContactUsCtrl'
+    })
     .state('movie-inside', {
-    url: "/movie-inside",
-    templateUrl: "views/template.html",
-    controller: 'MovieInsideCtrl'
-  })
+      url: "/movie-inside",
+      templateUrl: "views/template.html",
+      controller: 'MovieInsideCtrl'
+    })
     .state('overview', {
-    url: "/overview",
-    templateUrl: "views/template.html",
-    controller: 'OverviewCtrl'
-  });
+      url: "/overview",
+      templateUrl: "views/template.html",
+      controller: 'OverviewCtrl'
+    });
   $urlRouterProvider.otherwise("/");
   $locationProvider.html5Mode(isproduction);
 });
@@ -93,69 +93,99 @@ firstapp.directive('img', function($compile, $parse) {
   };
 });
 firstapp.directive('fancybox', function($compile, $parse) {
-    return {
-        restrict: 'EA',
-        replace: false,
-        link: function($scope, element, attrs) {
-            $element = $(element);
-            console.log("Checking Fancybox");
-            setTimeout(function() {
-                $(".various").fancybox({
-                    maxWidth: 800,
-                    maxHeight: 600,
-                    fitToView: false,
-                    width: '70%',
-                    height: '70%',
-                    autoSize: false,
-                    closeClick: false,
-                    openEffect: 'none',
-                    closeEffect: 'none'
-                });
-            }, 100);
-        }
-    };
+  return {
+    restrict: 'EA',
+    replace: false,
+    link: function($scope, element, attrs) {
+      $element = $(element);
+      console.log("Checking Fancybox");
+      setTimeout(function() {
+        $(".various").fancybox({
+          maxWidth: 800,
+          maxHeight: 600,
+          fitToView: false,
+          width: '70%',
+          height: '70%',
+          autoSize: false,
+          closeClick: false,
+          openEffect: 'none',
+          closeEffect: 'none'
+        });
+      }, 100);
+    }
+  };
 });
 firstapp.directive('autoHeight', function($compile, $parse) {
-    return {
-        restrict: 'EA',
-        replace: false,
-        link: function($scope, element, attrs) {
-            var $element = $(element);
-            var windowHeight = $(window).height();
-            var addHeight = function() {
-                $element.css("min-height", windowHeight);
-            };
-            addHeight();
-        }
-    };
+  return {
+    restrict: 'EA',
+    replace: false,
+    link: function($scope, element, attrs) {
+      var $element = $(element);
+      var windowHeight = $(window).height();
+      var addHeight = function() {
+        $element.css("min-height", windowHeight);
+      };
+      addHeight();
+    }
+  };
 });
 firstapp.directive('fancyboxBox', function($document) {
-    return {
-        restrict: 'EA',
-        replace: false,
-        link: function(scope, element, attr) {
-            var $element = $(element);
-            var target;
-            if (attr.rel) {
-               target = $("[rel='" + attr.rel + "']");
-            } else {
-                target = element;
-            }
+  return {
+    restrict: 'EA',
+    replace: false,
+    link: function(scope, element, attr) {
+      var $element = $(element);
+      var target;
+      if (attr.rel) {
+        target = $("[rel='" + attr.rel + "']");
+      } else {
+        target = element;
+      }
 
-            target.fancybox({
-                openEffect: 'fade',
-                closeEffect: 'fade',
-                closeBtn: true,
-                helpers: {
-                    media: {}
-                }
-            });
+      target.fancybox({
+        openEffect: 'fade',
+        closeEffect: 'fade',
+        closeBtn: true,
+        helpers: {
+          media: {}
         }
-    };
+      });
+    }
+  };
 });
+firstapp.directive('scrolldown', function($compile, $parse) {
+  return {
+    restrict: 'EA',
+    replace: false,
+    link: function($scope, element, attrs) {
+      var $element = $(element);
+      // var windowHeight = $(window).height();
+      $scope.scrollDown = function() {
+        $('html,body').animate({
+            scrollTop: $(".second").offset().top
+          },
+          'slow');
+      }
+    }
+  };
+});
+firstapp.directive('scrollToItem', function() {
+  return {
+    restrict: 'A',
+    scope: {
+      scrollTo: "@"
+    },
+    link: function(scope, $elm, attr) {
 
-
-firstapp.config(function ($translateProvider) {
+      $elm.on('click', function() {
+        $('html,body').animate({
+          scrollTop: $(scope.scrollTo).offset().top
+        }, "slow");
+      });
+    }
+  }
+});
+firstapp.config(function($translateProvider) {
   $translateProvider.translations('en', LanguageEnglish);
   $translateProvider.translations('hi', LanguageHindi);
   $translateProvider.preferredLanguage('en');
